af43027c51a09142ec116c862fb6afe46ee8ed6c
Xi <smoring1995@gmail.com>
2021-01-07 21:53:08 -0800

    First commit

---
+++ src/main/java/com/rosseneri/jiaxi/JiaxiApplication.java
@@ -0,0 +1,13 @@
+package com.rosseneri.jiaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rosseneri/jiaxi/JiaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosseneri.jiaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
